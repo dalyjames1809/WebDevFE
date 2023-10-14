3073bb418bc120c0e18f7ce688d82c0514361bb1
24741213 <24741213@sun.ac.za>
2023-10-14 20:03:01 +0200

    SignOutNew

--- src/SignOutPopUp.js
+++ src/SignOutPopUp.js
@@ -1,30 +1,24 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch, Link, Routes } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
 import './ConfirmationDialog.css';
-import Home from './Home';
-
-function Direct() {
-  return (
-    <Router>
-      <Routes>
-        {/* Home Page */}
-        <Route path="/loginPage" element={<Home />} />
-      </Routes>
-    </Router>
-  );
-}
 
 function ConfirmationDialog({ handleClose, handleConfirm }) {
   const navigate = useNavigate();
+
+  // Define a function to navigate to the login page
   const navigateToLoginPage = () => {
-    navigate('/loginPage');
+    navigate('/');
   };
+
   return (
     <div className="confirmation-dialog">
       <h2>Are you sure you want to sign out?</h2>
-      <button className="confirm-button" onClick={navigateToLoginPage}>Yes</button>
-      <button className="cancel-button" onClick={handleClose}>No</button>
+      <button className="confirm-button" onClick={navigateToLoginPage}>
+        Yes
+      </button>
+      <button className="cancel-button" onClick={handleClose}>
+        No
+      </button>
     </div>
   );
 }
