64508a6a2f457a565b4b2fb05ac2ffed2a848676
DCoetzee <101389904+DCoetzee@users.noreply.github.com>
2023-10-16 18:50:49 +0200

    Update UserContext.js with name

--- src/UserContext.js
+++ src/UserContext.js
@@ -5,9 +5,10 @@ export const UserContext = createContext();
 export function UserProvider({ children }) {
   const [username, setUsername] = useState(null);
   const [userToken, setUserToken] = useState(null);
+  const [name, setName] = useState(null);
 
   return (
-    <UserContext.Provider value={{ username, setUsername, userToken, setUserToken }}>
+    <UserContext.Provider value={{ username, setUsername, userToken, setUserToken, name, setName }}>
       {children}
     </UserContext.Provider>
   );
